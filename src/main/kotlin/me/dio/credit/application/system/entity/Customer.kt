5d9db02402117d79125306dd725c43a4ae98954c
Grisoste <gabrielgrisostedesouza@gmail.com>
2023-12-15 06:13:38 -0300

    commit CRUD Endpoints CustomerResource - (save)

--- src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -1,26 +1,29 @@
 package me.dio.credit.application.system.entity
 
 import jakarta.persistence.*
+import java.math.BigDecimal
 
 @Entity //Vai se tornar uma tabela no db
 //@Table(name = "Cliente") //Posso alterar o nome da tablea
 data class Customer(
     @Column(nullable = false)
-        var firstName: String = "", //Nao pode ser nulo
+    var firstName: String = "", //Nao pode ser nulo
     @Column(nullable = false)
-        var lastName: String = "",
+    var lastName: String = "",
     @Column(nullable = false, unique = true)
-        val cpf: String, //dizendo que o email so pode ser unico
+    val cpf: String, //dizendo que o email so pode ser unico
     @Column(nullable = false, unique = true)
-        var email: String = "",
+    var email: String = "",
     @Column(nullable = false)
-        var password: String = "",
+    val income: String = BigDecimal.ZERO,
+    @Column(nullable = false)
+    var password: String = "",
     @Column(nullable = false) @Embedded
-        var adress: Address = Address(),
+    var adress: Address = Address(),
     @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.REMOVE), mappedBy = "customer")
-        var credits: List<Credit> = mutableListOf(),
+    var credits: List<Credit> = mutableListOf(),
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long? = null
+    val id: Long? = null
     //@Id to querendo dizer que é a chave primaria
     //@GeneratedValue é pra gerar os valores
 )
