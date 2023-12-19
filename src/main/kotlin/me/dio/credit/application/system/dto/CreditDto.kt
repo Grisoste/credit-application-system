d86368afadc40c0e9b773435720320a3f026fc2e
Grisoste <gabrielgrisostedesouza@gmail.com>
2023-12-19 11:58:46 -0300

    Desafio Concluido

--- src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
@@ -1,6 +1,7 @@
 package me.dio.credit.application.system.dto
 
 import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotNull
 import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.entity.Customer
@@ -12,10 +13,24 @@ data class CreditDto(
     @field:Future val dayFirstOfInstallment: LocalDate,
     @field:NotNull(message = "Valor nao pode ser nulo")
         @field:Future(message = "Precisa ser uma data no futuro")
-            val numberOfInstallments: Int,
+            @field:Max(value = 48, message = "Número de parcelas deve ser no máximo 48")
+                val numberOfInstallments: Int,
     @field:NotNull(message = "Valor nao pode ser nulo") val customerId: Long
 
 ) {
+    init {
+        validateDates()
+    }
+
+    private fun validateDates() {
+        val currentDate = LocalDate.now()
+        val maxDate = currentDate.plusMonths(3)
+
+        if (dayFirstOfInstallment.isBefore(currentDate) || dayFirstOfInstallment.isAfter(maxDate)) {
+            throw IllegalArgumentException("A data da primeira parcela deve ser no máximo 3 meses após o dia atual")
+        }
+    }
+
     fun toEntity(): Credit = Credit(
         creditValue = this.creditValue,
         dayFirstInstallment = this.dayFirstOfInstallment,
